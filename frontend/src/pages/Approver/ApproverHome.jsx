ea6cad93a8a74d483887d8e673d60b663a6fd0fe
jrppena <jppena@up.edu.ph>
2023-05-29 09:13:40 +0800

    feat: added route for approver

---
+++ frontend/src/pages/Approver/ApproverHome.jsx
@@ -0,0 +1,13 @@
+import { useState,useEffect } from "react";
+
+return(
+    <>
+    <h3>SEARCH STUDENTS</h3>
+    <input placeholder="SEARCH"/>
+    <button>SORT</button>
+    <button>FILTER</button>
+    <table>
+        
+    </table>
+    </>
+)
