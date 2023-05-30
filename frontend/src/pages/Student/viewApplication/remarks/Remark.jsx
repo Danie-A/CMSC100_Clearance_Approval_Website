a1e78ccf82ebea7e9ca1ee550be121a2d44fe01b
Danielle Araez <draraez@up.edu.ph>
2023-05-30 13:15:04 +0800

    ui: minor color changes

--- frontend/src/pages/Student/viewApplication/remarks/Remark.jsx
+++ frontend/src/pages/Student/viewApplication/remarks/Remark.jsx
@@ -4,7 +4,9 @@ import { AiOutlineComment } from 'react-icons/ai';
 function Remark(props) {
     return (
         <div className="remark-item">
-            <AiOutlineComment className="mr-2" color={'green'} style={{ marginRight: '8px' }} />
+            <br />
+            <AiOutlineComment color={'orange'} style={{ marginRight: '8px' }} />
+            <br></br>
             <p>Remark: {props.remark}</p>
             <p>Step: {props.step}</p>
             <p>Date: {props.date}</p>
