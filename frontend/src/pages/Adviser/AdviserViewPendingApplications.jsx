3c28fc6fec31c0ba6f67251ef70b3b9d59cd8c61
Danielle Araez <draraez@up.edu.ph>
2023-06-03 18:51:55 +0800

    fix: tried fixing approver fetch methods

--- frontend/src/pages/Adviser/AdviserViewPendingApplications.jsx
+++ frontend/src/pages/Adviser/AdviserViewPendingApplications.jsx
@@ -2,32 +2,39 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 function AdviserViewPendingApplications() {
+
   const [pendingApplications, setPendingApplications] = useState([]);
+  const [advisees, setAdvisees] = useState([]);
+
+  // get advisees with pending applications
+
   useEffect(() => {
     const e = async () => { // still not working
       await fetch("http://localhost:3001/get-pending-applications-adviser", { method: "GET", credentials: "include" })
         .then((response) => response.json())
-        .then((body) => setPendingApplications(body.request));
+        .then((body) => {
+          setPendingApplications(body.applications)
+          console.log('body applications is', body.applications)
+          console.log('body advisees are', body.advisees)
+          setAdvisees(body.advisees);
+
+        });
     };
     e();
   }, []);
 
-  useEffect(() => console.log(pendingApplications));
 
   return (
     <>
       <h3>{"Adviser's View Pending Applications"}</h3>
       {pendingApplications.map((application, index) => (
-        <table>
-          <tbody>
-            <tr key={index}>
-              {application.owner}
-            </tr>
-          </tbody>
-        </table>
+        <div key={index}> {application.owner}</div>
+
+
       ))}
     </>
   );
 }
+
 
 export default AdviserViewPendingApplications;
