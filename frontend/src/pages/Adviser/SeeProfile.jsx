83f8d19f09e1c8210a396c6c0d0e900f9ceb5a46
Danielle Araez <draraez@up.edu.ph>
2023-06-04 06:20:21 +0800

    feat: can now view remarks of adviser to student

--- frontend/src/pages/Adviser/SeeProfile.jsx
+++ frontend/src/pages/Adviser/SeeProfile.jsx
@@ -2,11 +2,37 @@ import React from 'react';
 import ReactModal from 'react-modal';
 import ReturnPopUp from './ReturnPopUp';
 import { useState } from 'react';
-
+import Remark from '../Student/viewApplication/remarks/Remark';
+import { BiCommentDetail } from 'react-icons/bi';
 
 export default function SeeProfile({ handleCloseModal, student }) {
+    const username = localStorage.getItem("username"); // username of adviser
     // pending application id
     const applicationId = student.open_application._id;
+
+    // for open adviser remarks modal
+    const handleOpenRemark = () => {
+        setShowRemark(true);
+    };
+
+    const handleCloseRemark = () => {
+        setShowRemark(false);
+    };
+
+    const [showRemark, setShowRemark] = useState(false);
+
+
+    // for open student submissions modal
+    const handleOpenSubmission = () => {
+        setShowSubmission(true);
+    };
+
+    const handleCloseSubmission = () => {
+        setShowSubmission(false);
+    };
+
+    const [showSubmission, setShowSubmission] = useState(false);
+
 
     // for react modal to view application of student
     const handleOpenModal2 = () => {
@@ -45,7 +71,7 @@ export default function SeeProfile({ handleCloseModal, student }) {
         if (step === 1 && status === 'Pending') {
             // show github link
             return (<div>
-                <p>GitHub Link: <a href={github_link} target="_blank" rel="noopener noreferrer">{github_link}</a></p>
+                <p><strong>GitHub Link:</strong>  <a href={github_link} target="_blank" rel="noopener noreferrer">{github_link}</a></p>
             </div>);
 
         } else {
@@ -53,13 +79,66 @@ export default function SeeProfile({ handleCloseModal, student }) {
             student_remark = latestSubmission['student_remark']; // since student_remark is not in step 1
 
             return (<div>
-                <p>GitHub Link: <a href={github_link} target="_blank" rel="noopener noreferrer">{github_link}</a></p>
+                <p><strong>GitHub Link:</strong>  <a href={github_link} target="_blank" rel="noopener noreferrer">{github_link}</a></p>
                 <p>{student_remark}</p>
             </div>);
 
         }
 
     }
+
+    // remarks of adviser to student
+    function viewRemarks(remarks) {
+
+        const sortedRemarks = remarks.sort((a, b) => {
+            if (a._id > b._id) {
+                return -1; // Sort in descending order
+            } else if (a._id < b._id) {
+                return 1;
+            }
+            return 0;
+        });
+
+        return (
+            <div className='whole-container'>
+
+                <button type="button" className="btn-close btn-right" aria-label="Close" onClick={handleCloseModal}></button>
+
+                <h5>Returned Remarks</h5>
+
+                <div className="remark-container">
+
+                    <div>
+                        {sortedRemarks.map((remark, index) => (
+                            <Remark key={index} remark={remark.remarks} step={remark.step} date={remark.date.slice(0, 10)} commenter={username} />
+                        ))}
+                    </div>
+
+                </div>
+
+            </div>
+
+        );
+
+    }
+
+    // list of submissions of student regarding the application
+    function viewSubmissions(student_submissions) {
+        return (
+            <div>
+                {student_submissions.map((submission, index) => (
+                    <div key={index}>
+                        <p>{submission.remark}</p>
+                        <p>{submission.step}</p>
+                        <p>{submission.date}</p>
+                        <p>{username}</p>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+
+
 
     const handleApprove = async (e) => {
         e.preventDefault();
@@ -77,6 +156,8 @@ export default function SeeProfile({ handleCloseModal, student }) {
 
     }
 
+
+
     // code to convert Mongoose date and time to UTC+8 Philippine time
     const timestamp = new Date(date);
 
@@ -91,27 +172,47 @@ export default function SeeProfile({ handleCloseModal, student }) {
     };
 
     const formattedDateTime = timestamp.toLocaleString("en-US", chinaTimeOptions);
-    console.log(formattedDateTime);
 
     return (<>
         <button type="button" className="btn-close btn-right" aria-label="Close" onClick={handleCloseModal}></button>
-        <div>Name: {student.first_name} {student.last_name}</div>
-        <div>Student Number: {student.student_number}</div>
-        <div>Email: {student.email}</div>
 
-        <div>Date Submitted: {formattedDateTime}</div>
-        {showLink()}
+        <div className="whole-container">
 
+            <br></br>
+            <h4>Student Information</h4>
+            <div><strong>Name:</strong> {student.first_name} {student.last_name}</div>
+            <div><strong>Student Number:</strong>  {student.student_number}</div>
+            <div><strong>Email:</strong>  {student.email}</div>
+            <br />
+            <h4>Student Clearance Application</h4>
+            <div><strong>Date Submitted:</strong>  {formattedDateTime}</div>
+            {showLink()}
+            <br />
+            <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+                <button onClick={handleOpenRemark}><BiCommentDetail className="mr-2" style={{ marginRight: '8px' }} />View Adviser Remarks</button>
+                {/* showRemarks(student.open_application.remarks) */}
+                <ReactModal
+                    isOpen={showRemark}
+                    contentLabel="Remarks"
+                    onRequestClose={handleCloseRemark}
+                    appElement={document.getElementById('root')} // Set the app element
+                >
+                    {viewRemarks(student.open_application.remarks)}
 
-        <button>View Adviser Remarks</button>
-        <button>View Student Submissions</button>
+                </ReactModal>
 
-        <br /><br />
+                <button onClick={handleOpenSubmission}>View Student Submissions</button>
 
-        <button onClick={handleApprove}>Approve</button>
+                {/*  showSubmissions(student.open_application.student_submissions) */}
 
+            </div>
+            <br /><br />
+            <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+                <button onClick={handleApprove}>Approve</button>
+                <button onClick={handleOpenModal2}>Return</button>
+            </div>
 
-        <button onClick={handleOpenModal2}>Return</button>
+        </div>
         {/* Pop Up Another Modal to Add Remark */}
         <ReactModal
             style={modalStyle}
