9921ad88330a123b9ae7b2d63899d163497224e6
Danielle Araez <draraez@up.edu.ph>
2023-06-05 01:57:47 +0800

    fix: upload csv file button clickable and ui

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,10 @@ import setUpRoutes from "./routes.js";
 const app = express();
 
 // connect to Mongo DB
-await mongoose.connect("mongodb://127.0.0.1:27017/AUTH");
-const DATABASE_URI = "mongodb://127.0.0.1:27017/AUTH";
-// const DATABASE_URI = "mongodb+srv://jpsabile:VUNVL7QcJ2tYPbZr@jpsabile.nvysktb.mongodb.net/clearME?retryWrites=true&w=majority";
+// await mongoose.connect("mongodb://127.0.0.1:27017/AUTH");
+// const DATABASE_URI = "mongodb://127.0.0.1:27017/AUTH";
+const DATABASE_URI =
+  "mongodb+srv://jpsabile:VUNVL7QcJ2tYPbZr@jpsabile.nvysktb.mongodb.net/clearME?retryWrites=true&w=majority";
 await mongoose
   .connect(DATABASE_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connect to MongoDB"))
@@ -30,7 +31,10 @@ app.use(cookieParser());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,POST");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Access-Control-Allow-Methods,Origin,Accept,Content-Type,X-Requested-With,Cookie");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers,Access-Control-Allow-Methods,Origin,Accept,Content-Type,X-Requested-With,Cookie"
+  );
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
--- frontend/src/pages/Admin/AdminHome.jsx
+++ frontend/src/pages/Admin/AdminHome.jsx
@@ -7,7 +7,10 @@ function AdminHome() {
     try {
       const formData = new FormData();
       formData.append("file", selectedFile);
-      fetch("http://localhost:3001/upload-csv-file", { method: "POST", body: formData })
+      fetch("http://localhost:3001/upload-csv-file", {
+        method: "POST",
+        body: formData,
+      })
         .then((res) => res.json())
         .then((body) => {
           if (body.success) alert("Successfully uploaded CSV file!");
@@ -20,18 +23,26 @@ function AdminHome() {
 
   return (
     <>
-      <div id="welcome-admin">
-        <h3>{"Welcome admin"}</h3>
-        <span id="csv">Csv file: </span> <br />
-        <form></form>
-        
-        <label for="file-upload" class="button-stylee">
-        
+      <div
+        id="welcome-admin"
+        className="container d-flex flex-column align-items-start"
+      >
+        <h3 className="my-4">{"Welcome admin"}</h3>
+        <span>CSV File to Map Students with their Adviser: </span>
+        <label class="custom-file-upload">
+          <input
+            type="file"
+            name="file"
+            onChange={(e) => setSelectedFile(e.target.files[0])}
+          />
+          Click to Upload CSV
         </label>
-
-        <input type="file" name="file" onChange={(e) => setSelectedFile(e.target.files[0])} />
         <button onClick={handleCSVUpload}>Submit</button>
-        <img id="image" src="https://i.pinimg.com/originals/1b/0f/b0/1b0fb0ed95b7ee77ac662af8adcc29e7.png" alt="admin-home"></img>
+        <img
+          id="image"
+          src="https://i.pinimg.com/originals/1b/0f/b0/1b0fb0ed95b7ee77ac662af8adcc29e7.png"
+          alt="admin-home"
+        ></img>
       </div>
     </>
   );
